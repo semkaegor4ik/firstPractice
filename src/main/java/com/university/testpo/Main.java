831e3217ab7800eb442d0a6b066acb8dbcba4b98
semkaegor4ik <267902.egor@gmail.com>
2020-09-19 11:25:25 +0400

    with first test

--- src/main/java/com/university/testpo/Main.java
+++ src/main/java/com/university/testpo/Main.java
@@ -1,7 +1,19 @@
 package com.university.testpo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+
     public static void main(String[] args) {
+        StartPage startPage = new StartPage();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\semenovea\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(startPage.getUrl());
 
     }
 }
---
+++ src/main/java/com/university/testpo/StartPage.java
@@ -0,0 +1,9 @@
+package com.university.testpo;
+
+import lombok.Data;
+
+@Data
+public class StartPage{
+    private final String url ="https://beru.ru/";
+
+}
